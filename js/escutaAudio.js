beaac175bcef93478031bf59918441c04acad11b
Filipe <fbianchi.andrade@gmail.com>
2023-11-27 13:09:33 -0300

    removendo comentarios

--- js/escutaAudio.js
+++ js/escutaAudio.js
@@ -1,31 +1,22 @@
-// Criando uma instância do contexto de áudio
 const audioContext = new (window.AudioContext || window.webkitAudioContext)();
 const cronometro = document.getElementById('apresentacao');
-// Conectando o elemento de áudio ao contexto de áudio
 const source = audioContext.createMediaElementSource(loFi);
-
-// Criando um AnalyserNode
 const analyser = audioContext.createAnalyser();
 
-// Conectando o AnalyserNode entre o elemento de áudio e o destino do áudio
 source.connect(analyser);
 analyser.connect(audioContext.destination);
+analyser.fftSize = 256; 
 
-// Inicializando o AnalyserNode
-analyser.fftSize = 256; // Definindo o tamanho da FFT (Fast Fourier Transform)
-
-// Obtendo os dados de frequência
 const bufferLength = analyser.frequencyBinCount;
 const dataArray = new Uint8Array(bufferLength);
 
-// Função para atualizar os dados de frequência e realizar a análise
 function analyze() {
     analyser.getByteFrequencyData(dataArray);
     var array  = dataArray.filter(numero => numero > 40 && numero < 80);
     array = array.reduce((soma, valor) => soma + valor, 0) / array.length;
     var limiarBumbo = 50;
 
-    cronometro.style.transition = '0.6s'
+    cronometro.style.transition = '0.5s'
     
     var alteracaoAoTocarBumbo = `${array}px solid rgba(0,0,0, 0.7)`;
     var alteracaoSemBumbo = '1px solid rgba(0,0,0, 0.7)'
